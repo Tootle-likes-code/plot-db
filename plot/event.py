8e81af1593a26f9e5f31f6cb489c915401a552cc
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-07-02 14:19:46 +0100

    Created initial event and linked subclass.

---
+++ plot/event.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+
+from plot.character import Character
+from plot.location import Location
+from plot.plot_date import PlotDate
+
+
+@dataclass
+class Event:
+    name: str
+    description: str
+    location: Location
+    plot_date: PlotDate
+    involved_characters: set(Character) = field(default_factory=list)
+
+
+@dataclass
+class LinkedEvent(Event):
+    triggering_events: set(Event) = field(default_factory=list)
+    following_events: set(Event) = field(default_factory=list)
