076ff4951c8a0aeb5611864e59c6f1cb70433458
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-07-02 14:28:43 +0100

    Added pylint

--- plot/character.py
+++ plot/character.py
@@ -1,7 +1,12 @@
+""" Manages characters. """
+
 from dataclasses import dataclass
 
 
 @dataclass
 class Character:
+    """
+    A character data class.
+    """
     name: str
     description: str
