83bfa4621876a72926f04cd0d9550292d1009884
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-07-02 14:19:03 +0100

    Created base custom date data class for custom dates.

---
+++ plot/plot_date.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class PlotDate:
+    year: int
+    month: Optional[int] = None
+    day: Optional[int] = None
+    hour: Optional[int] = None
+
+    def __init__(self, year, month=None, day=None, hour=None):
+        if not month and day:
+            raise ValueError("Must be given a month as well as a day.")
+
+        if not day and hour:
+            raise ValueError("Must be given a day as well as an hour.")
+
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
+
+
+    def __str__(self) -> str:
+        date = f"{self.year}"
+        date += f"{f'-{self.month:02}' if self.month else ''}"
+        date += f"{f'-{self.day:02}' if self.day else ''}"
+        date += f"{f'T{self.hour:02}' if self.hour else ''}"
+        return date
