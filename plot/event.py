13e16a9a20ad9081d4fb9ef977ff1847e56a8d79
Jason Kennelly <tootle_likes_code@hotmail.com>
2022-07-02 14:41:55 +0100

    Added pylint

--- plot/event.py
+++ plot/event.py
@@ -1,3 +1,5 @@
+""" Represents events within a plot. """
+
 from dataclasses import dataclass, field
 
 from plot.character import Character
@@ -7,18 +9,40 @@ from plot.plot_date import PlotDate
 
 @dataclass
 class Event:
+    """ A thing that happened at a time and a place, likely with characters. """
     name: str
     description: str
     location: Location
     plot_date: PlotDate
-    involved_characters: set[Character] = field(default_factory=list)
+    involved_characters: set[Character] = field(default_factory=set)
 
 
 @dataclass
 class LinkedEvent(Event):
-    triggering_events: set[Event] = field(default_factory=list)
-    following_events: set[Event] = field(default_factory=list)
+    """
+    A thing that happened at a time and a place, likely with characters, with links to events
+    that influenced this event and those that came abouts because of it.
+    """
+    triggering_events: set[Event] = field(default_factory=set)
+    following_events: set[Event] = field(default_factory=set)
 
     @staticmethod
-    def convert_event(event: Event):
-        return LinkedEvent(event.name, event.description, event.location, event.plot_date)
+    def convert_event(event: Event, triggering_events: set[Event] = None,
+                      following_events: set[Event] = None):
+        """
+        Converts an Event into a LinkedEvent
+        :param event: The Event you wish to convert.
+        :param triggering_events: Any events that you want this LinkedEvent to have been the
+        result of. If left blank, initialises to set.
+        :param following_events: Any events that were caused directly because of this event.
+        If left blank, initialises to an empty set.
+        :return: The new LinkedEvent.
+        """
+        if not triggering_events:
+            triggering_events = set([])
+
+        if not following_events:
+            following_events = set([])
+
+        return LinkedEvent(event.name, event.description, event.location, event.plot_date,
+                           triggering_events, following_events)
--- plot/location.py
+++ plot/location.py
@@ -1,3 +1,5 @@
+""" Manages locations of a plot. """
+
 from abc import ABC
 from dataclasses import dataclass
 from typing import Optional
@@ -5,10 +7,12 @@ from typing import Optional
 
 @dataclass
 class Place(ABC):
+    """ The root of locations. """
     name: str
     description: str
 
 
 @dataclass
 class Location(Place):
+    """ The implementation of Place, which is aware of it's parent location. """
     parent_location: Optional[Place]
